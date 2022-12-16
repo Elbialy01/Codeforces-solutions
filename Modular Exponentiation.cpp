093fc0320a9b0406bf670d5a4874dc8d0de9c800
Mahmoud Elbialy <119286962+Elbialy01@users.noreply.github.com>
2022-12-17 00:04:53 +0200

    Create Modular Exponentiation.cpp

---
+++ Modular Exponentiation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+#include <math.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+long long int  n ,k,counter=0;
+cin>>n>>k;
+long long int  p=pow(2,n);
+//vcout<<p;
+cout<<k%p;
+
+
+}
+
+
+
+
