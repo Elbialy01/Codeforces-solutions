6b1adf458fb906766de0117a0198af1714fe316a
Mahmoud Elbialy <119286962+Elbialy01@users.noreply.github.com>
2022-12-17 10:31:49 +0200

    Create  EhAb AnD gCd.cpp

---
+++ EhAb AnD gCd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+    if(n%2==0)
+        cout<<n/2<<" "<<n/2;
+    else cout<<n-1<<" "<<1;
+    cout<<endl;
+
+    }
+    return 0;
+}
